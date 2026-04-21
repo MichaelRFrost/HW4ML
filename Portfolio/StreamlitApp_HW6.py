e8776553232cf628d35ac7b5f827465634723abe
MichaelRFrost <michael.r.frost@tcu.edu>
2026-04-21 13:21:20 -0500

    Reorganize imports and improve session management

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -107,14 +107,14 @@ def call_model_api(input_df):
 
     try:
         # For regression
-        # raw_pred = predictor.predict(input_df)
-        # pred_val = pd.DataFrame(raw_pred).values[-1][0]
-        # return round(float(pred_val), 4), 200
-        # For classification
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
-        mapping = {-1: "SELL", 0: "HOLD", 1: "BUY"}
-        return mapping.get(pred_val), 200
+        return round(float(pred_val), 4), 200
+        # For classification
+        # raw_pred = predictor.predict(input_df)
+        # pred_val = pd.DataFrame(raw_pred).values[-1][0]
+        # mapping = {-1: "SELL", 0: "HOLD", 1: "BUY"}
+        # return mapping.get(pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
 
